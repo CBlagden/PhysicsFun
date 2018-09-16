ba749fe1d4987a7af3e6c2305e9cb4c7a1451f10
Chase Blagden <chase.blagden@gmail.com>
2018-09-16 15:55:21 -0700

    finished lab

--- track_balls.py
+++ track_balls.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-import scipy.interpolate
 import argparse
 import numpy as np
 
@@ -14,6 +13,10 @@ def graph(x, y, title, xlabel='', ylabel=''):
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.margins(0)
+
+
+def percent_error(experimental_val, true_val):
+    return abs(experimental_val - true_val) / abs(true_val) * 100
 
 
 class ObjectTracker:
@@ -74,21 +77,28 @@ class ObjectTracker:
         
         img_resized = self.resize_img(img)
 
-        img_hsv = cv2.cvtColor(img_resized, cv2.COLOR_RGB2HSV)
-        mask = cv2.inRange(img_hsv, self.lower_hsv_range, self.upper_hsv_range)
+        img_hsv = cv2.cvtColor(img_resized, 
+            cv2.COLOR_RGB2HSV)
+        mask = cv2.inRange(img_hsv, 
+            self.lower_hsv_range, 
+            self.upper_hsv_range)
         mask = cv2.erode(mask, None, iterations=4)
         mask = cv2.dilate(mask, None, iterations=4)
 
         cv2.imshow('mask', mask)
 
-        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        _, contours, _ = cv2.findContours(mask, 
+            cv2.RETR_TREE, 
+            cv2.CHAIN_APPROX_SIMPLE)
         x = None
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
 
             if radius > self.min_radius:
-                cv2.circle(img_resized, (int(x), int(y)), int(radius), (0, 255, 255), 2)    
+                cv2.circle(img_resized,
+                 (int(x), int(y)), 
+                    int(radius), (0, 255, 255), 2)    
             else:
                 x = None
 
@@ -98,15 +108,17 @@ class ObjectTracker:
         pass
 
     def pixels_to_meters(self, pixels):
-        return pixels * 2.1 / 500
+        return pixels * 2.1 / 500.0
 
     def get_new_pos_vel_and_accel(self, times, positions, k=2):
-        pos_spl = scipy.interpolate.UnivariateSpline(x=times, y=positions, k=k, s=1e8)
-        vel_spl = pos_spl.derivative()
-        acc_spl = vel_spl.derivative()
 
-        x_new = np.linspace(self.start_time, self.end_time, num=1000)
-        return pos_spl(times), vel_spl(times), acc_spl(times), times
+        times = np.array(times) * 0.168
+
+        pos_poly = np.poly1d(np.polyfit(times, positions, deg=k))
+        vel_poly = np.polyder(pos_poly)
+        acc_poly = np.polyder(pos_poly, m=2)
+
+        return pos_poly(times), vel_poly(times), acc_poly(times), times
 
 
 class TennisBallTracker(ObjectTracker):
@@ -148,6 +160,19 @@ class TennisBallUpAndDown(ObjectTracker):
         return img[int(0.41*rows):int(0.67*rows), int(0.04*cols):int(0.43*cols)]
 
 
+class TennisBallWithBaseBallTracker(ObjectTracker):
+
+    def __init__(self, file):
+        super().__init__(file, start_time=12.5, end_time=16.1, 
+            lower_hsv_range=np.array([28.0, 89.0, 45.0]),
+            upper_hsv_range=np.array([154.0, 187.0, 255.0]),
+            min_radius=8.0)
+
+    def resize_img(self, img):
+        rows, cols, _ = img.shape
+        return img[int(0.4*rows):int(0.6*rows), int(0.04*cols):]
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--video', required=True, help="path to the video file")
@@ -160,7 +185,9 @@ def main():
         tracker = BaseBallTracker(args['video'])
     elif args['ball'] in ('tennisball', 't'):
         tracker = TennisBallTracker(args['video'])
-    else:
+    elif args['ball'] in ('with', 'w'):
+        tracker = TennisBallWithBaseBallTracker(args['video'])
+    elif args['ball'] in ('upanddown' 'u'):
         tracker = TennisBallUpAndDown(args['video'])
 
     times, positions = tracker.process_video(show=True)
@@ -168,6 +195,8 @@ def main():
     graph(times, pos, 'position', xlabel='Time (s)', ylabel='Position (m)')
     graph(times, vel, 'velocity', xlabel='Time (s)', ylabel='Velocity (m/s)')
     graph(times, accel, 'acceleration', xlabel='Time (s)', ylabel='Acceleration (m/s^2)')
+    print("Percent error: %f" % percent_error(accel[0], 9.81))
+
     plt.show()
 
 if __name__ == '__main__':
